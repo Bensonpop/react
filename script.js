0a1c43167d6ca88570a9835c7256bd5ee5ce8b57
unknown <comradedeepak2003@gmail.com>
2023-03-16 14:57:58 +0530

    jerin not committed

---
+++ script.js
@@ -0,0 +1,13 @@
+/*function add(a,b){
+ let sum=a+b;
+ console.log(sum)
+}
+add(2,4)*/
+/*const add=(a,b)=>{
+    console.log(a+b)
+}
+add(4,4)*/
+const multi=(a,b)=>{
+    console.log(a*b)
+}
+multi(4,4)
